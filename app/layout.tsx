0a851085c3d01f5a5883a51c93f94f056b9f6ce6
Prashuk Jain <29jainprashuk@gmail.com>
2023-08-25 12:52:09 +0530

    updated meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,83 +11,72 @@ const syne = Syne({
     weight: ["400", "500", "600", "700", "800"],
 });
 
-export const metadata: Metadata = {
-    metadataBase: new URL("https://www.seekvisualartist.com/"),
-    title: "Seek Visual Artist",
-    description:
-    "Hobby Designer and Software Engineer, currently at Neumann Kaffee Gruppe. Focused on immersive experiences, studying in Hamburg, Germany.",
-    generator: "Next.js",
-    applicationName: "Seek Visual Artist",
-    keywords: [
-        "Seek Visual Artist",
-        "SeekVA",
-        "Seek Design",
-        "Seek Visuals",
-        "freelance",
-        "developer",
-        "freelance developer",
-        "frontend",
-        "nextjs",
-        "astro",
-        "react",
-        "frontend developer",
-        "frontend engineer",
-        "creative",
-        "creative developer",
-        "creative engineer",
-        "tech",
-        "germany",
-        "software",
-        "software developer",
-        "portfolio",
-        "frontend developer portfolio",
-        "creative developer portfolio",
-        "creative engineer portfolio",
-        "software developer portfolio",
-        "frontend engineer portfolio",
-    ],
-    colorScheme: "dark",
-    openGraph: {
-        title: "Seek Visual Artist - Designer and Developer",
-        description:
-      "Hobby Designer and Software Engineer, currently at Neumann Kaffee Gruppe. Focused on immersive experiences, studying in Hamburg, Germany.",
-        url: "https://www.seekvisualartist.com/",
-        siteName: "www.seekvisualartist.com",
-        images: [
-            {
-                url: "./public/metadata.jpg",
-                width: 1200,
-                height: 630,
-                alt: "Seek Visual Artist - Designer and Developer",
-            },
-        ],
-        locale: "en-US",
-        type: "website",
-    },
-    twitter: {
-        card: "summary_large_image",
-        title: "Seek Visual Artist - Designer and Developer",
-        description:
-      "Hobby Designer and Software Engineer, currently at Neumann Kaffee Gruppe. Focused on immersive experiences, studying in Hamburg, Germany.",
-        creator: "SeekVFX",
-        creatorId: "0000000000",
-        images: ["./public/metadata.jpg"],
-    },
-    robots: {
-        index: true,
-        follow: true,
-        nocache: false,
-        googleBot: {
-            index: true,
-            follow: false,
-            noimageindex: true,
-            "max-video-preview": -1,
-            "max-image-preview": "large",
-            "max-snippet": -1,
-        },
-    },
-    category: "technology",
-};
+// export const metadata: Metadata = {
+//     metadataBase: new URL("https://www.seekvisualartist.com/"),
+//     title: "Seek Visual Artist",
+//     description:
+//     "Hobby Designer and Software Engineer, currently at Neumann Kaffee Gruppe. Focused on immersive experiences, studying in Hamburg, Germany.",
+//     generator: "Next.js",
+//     applicationName: "Seek Visual Artist",
+//     keywords: [
+//         "Dr. Prateek Agrawal",
+//         "LPU",
+//         "IEEE CIS LPU",
+//         "Deputy Dean LPu",
+//         "HOS lpu",
+//         "LEAder",
+//         "CSE lpu",
+//         "Austria",
+//         "prateek",
+//         "profesor prateek",
+//         "Portfolio",
+//         "Lovely Proffesional university",
+//         "Lpu",
+//         "frontend developer portfolio",
+        
+//     ],
+//     colorScheme: "dark",
+//     openGraph: {
+//         title: "Seek Visual Artist - Designer and Developer",
+//         description:
+//       "Hobby Designer and Software Engineer, currently at Neumann Kaffee Gruppe. Focused on immersive experiences, studying in Hamburg, Germany.",
+//         url: "https://www.seekvisualartist.com/",
+//         siteName: "www.seekvisualartist.com",
+//         images: [
+//             {
+//                 url: "./public/metadata.jpg",
+//                 width: 1200,
+//                 height: 630,
+//                 alt: "Seek Visual Artist - Designer and Developer",
+//             },
+//         ],
+//         locale: "en-US",
+//         type: "website",
+//     },
+//     twitter: {
+//         card: "summary_large_image",
+//         title: "Seek Visual Artist - Designer and Developer",
+//         description:
+//       "Hobby Designer and Software Engineer, currently at Neumann Kaffee Gruppe. Focused on immersive experiences, studying in Hamburg, Germany.",
+//         creator: "SeekVFX",
+//         creatorId: "0000000000",
+//         images: ["./public/metadata.jpg"],
+//     },
+//     robots: {
+//         index: true,
+//         follow: true,
+//         nocache: false,
+//         googleBot: {
+//             index: true,
+//             follow: false,
+//             noimageindex: true,
+//             "max-video-preview": -1,
+//             "max-image-preview": "large",
+//             "max-snippet": -1,
+//         },
+//     },
+//     category: "technology",
+// };
 
 type RootLayoutProps = {
   children: ReactNode;
