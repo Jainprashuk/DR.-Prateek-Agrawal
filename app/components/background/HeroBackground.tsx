bc6951717324761c3cb7cef9d3d1a455addef566
Prashuk Jain <29jainprashuk@gmail.com>
2023-08-27 19:35:28 +0530

    bg-changes

--- app/components/background/HeroBackground.tsx
+++ app/components/background/HeroBackground.tsx
@@ -3,15 +3,15 @@ import React from "react";
 const HeroBackground: React.FC = () => {
     return (
         <div className="absolute inset-0">
-            {/* <video className="h-full w-full object-cover dark:hidden" autoPlay={true} playsInline={true} loop muted preload="auto">
+            <video className="h-full w-full object-cover dark:hidden" autoPlay={true} playsInline={true} loop muted preload="auto">
                 <source src="/bg2.mp4" type="video/mp4" />
             </video>
 
             <video className="h-full w-full object-cover hidden dark:flex" autoPlay={true} playsInline={true} loop muted preload="auto">
                 <source src="/bg3.mp4" type="video/mp4" />
-            </video> */}
+            </video>
             
-            <div className="bg-gray-100 dark:bg-blue-900 w-full h-full bg-gradient-to-t from-bg-dark to-transparent"/>
+            {/* <div className="bg-gray-100 dark:bg-blue-900 w-full h-full bg-gradient-to-t from-bg-dark to-transparent"/> */}
             
         </div>
     );
