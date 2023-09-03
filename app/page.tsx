69a1355fffdb79b0ba29f7be37b23c34c775d5c9
Prashuk Jain <29jainprashuk@gmail.com>
2023-09-04 00:29:57 +0530

    analytics

--- app/page.tsx
+++ app/page.tsx
@@ -87,6 +87,7 @@ export default function Home() {
                 <Experience/>
                 {/* <Testimonials/> */}
                 <Footer />
+                <Analytics/>
             </main>
             {/*</ScrollerMotion>*/}
         </>
